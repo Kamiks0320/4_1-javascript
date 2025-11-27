ff1537bbf848169a9cdd6f1a4958191d1d3048b0
Kamil <wegrzynk322@gmail.com>
2025-11-27 19:50:34 +0100

    Com4_1.1

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ const app = express();
 
 // define endpoint for exercise 1 here
 app.get('/math/circle/:r', (req, res) => {
-//TODO1  
+  //TODO1  
+  const {r} = req.params;
+
+  if(!r)
+  {
+    const missingpara = [];
+    if(!r) missingpara.push("r");
+
+    const error = `Brakuje potrzebnych parametrów get: ${missingpara.join(", ")}`;
+    return res.status(400).send(error);
+  }
+
+  const message = `Pole: ${3.14 * r * r}, obwód: ${2 * 3.14 * r}`;
+  res.type("text").send(message);
   res.json(result);
 });
 
